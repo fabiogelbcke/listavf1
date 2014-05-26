94844028be14d86be6bab1b5fb9a7fb16fbb45e9
Fabio Gelbcke <fabiogelbcke@Fabios-MacBook-Pro.local>
2014-05-25 22:03:44 -0300

    Initial Commit

---
+++ listavf1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  listavf1
+//
+//  Created by Fabio Gelbcke on 5/25/14.
+//  Copyright (c) 2014 Fabio Gelbcke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
